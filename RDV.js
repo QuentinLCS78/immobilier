facbf4547fea400180ec7d894f59c4592b8ef4aa
Antoine924 <131435139+Antoine924@users.noreply.github.com>
2024-05-27 21:39:40 +0200

    Add files via upload

---
+++ RDV.js
@@ -0,0 +1,8 @@
+// Valider les entrées ou gérer les interactions dynamiques
+document.querySelector('form').addEventListener('submit', function(event) {
+    // Vous pouvez ajouter des vérifications supplémentaires ici
+    if (!confirm('Confirmez-vous la date et l heure du rdv ?'))
+        {
+        event.preventDefault();
+    }
+});
